cb2813ae355388984f8c96b18972b31a91da7acb
unknown <rahulcr2797@gmail.com>
2026-02-13 00:06:33 +0530

    Provides audit trail.

--- src/main/java/logs/TransactionLogger.java
+++ src/main/java/logs/TransactionLogger.java
@@ -6,24 +6,31 @@ import java.sql.*;
 
 public class TransactionLogger {
 	
+	// Static method so it can be called without creating object
+	// Logs each transaction into DB
+	
 	public static void log(Connection con, int accountNumber, 
 							String type, 
 							BigDecimal amount, 
 							BigDecimal balanceAfter) throws SQLException{
 		
+		// SQL insert into transactions table
 		String sqlquery = "INSERT INTO transactions" + 
 							"(account_number, transaction_type, amount, balance_after)" +
 								"VALUES (?, ?, ?, ?)";
 		
-
-		PreparedStatement ps = con.prepareStatement(sqlquery);
-			
-		ps.setInt(1, accountNumber);
-		ps.setString(2, type);
-		ps.setBigDecimal(3, amount);
-		ps.setBigDecimal(4, balanceAfter);
-			
-		ps.executeUpdate();
+		// Prepare statement using existing connection
+		try(PreparedStatement ps = con.prepareStatement(sqlquery)){
+				
+			// Set values into placeholders
+			ps.setInt(1, accountNumber);
+			ps.setString(2, type);
+			ps.setBigDecimal(3, amount);
+			ps.setBigDecimal(4, balanceAfter);
+				
+			// Execute insert
+			ps.executeUpdate();
+		}
 			
 			
 	}
